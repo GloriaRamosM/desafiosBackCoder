9aa01cb92aa3f413b5ce76ab42b361a05e36bb4a
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-01-31 20:26:40 -0300

    desafio 1, ahora si realizado con classes

--- desafio.js
+++ desafio.js
@@ -1,95 +1,100 @@
-let productos = [];
-
-const getProductos = () => productos;
-
-function agregarProductos(
-  titulo,
-  descripcion,
-  precio,
-  rutaDeImagen,
-  codigo,
-  stock
-) {
-  if (
-    !titulo ||
-    !descripcion ||
-    !precio ||
-    !rutaDeImagen ||
-    !codigo ||
-    !stock
-  ) {
-    return console.log("Falta agregar datos a este producto");
+class ProductMannager {
+  constructor() {
+    this.productos = [];
   }
 
-  const productoExistente = productos.some(
-    (producto) => producto.codigo === codigo
-  );
+  getProductos = () => this.productos;
 
-  if (productoExistente) {
-    console.log("Producto ya existe , no se puede agregar");
-    return;
+  agregarProductos(titulo, descripcion, precio, rutaDeImagen, codigo, stock) {
+    if (
+      !titulo ||
+      !descripcion ||
+      !precio ||
+      !rutaDeImagen ||
+      !codigo ||
+      !stock
+    ) {
+      return console.log("Falta agregar datos a este producto");
+    }
+
+    const productoExistente = this.productos.some(
+      (producto) => producto.codigo === codigo
+    );
+
+    if (productoExistente) {
+      console.log("Producto ya existe , no se puede agregar");
+      return;
+    }
+
+    const producto = {
+      id: this.generarId(),
+      titulo: titulo,
+      descripcion: descripcion,
+      precio: precio,
+      rutaDeImagen: rutaDeImagen,
+      codigo: codigo,
+      stock: stock,
+    };
+
+    return this.productos.push(producto);
   }
 
-  const producto = {
-    id: generarId(),
-    titulo: titulo,
-    descripcion: descripcion,
-    precio: precio,
-    rutaDeImagen: rutaDeImagen,
-    codigo: codigo,
-    stock: stock,
-  };
+  generarId() {
+    let id = 0;
 
-  productos.push(producto);
+    if (this.productos.length === 0) {
+      id = 1;
+    } else {
+      id = this.productos[this.productos.length - 1].id + 1;
+    }
+    return id;
+  }
+
+  getProductoById(productoId) {
+    const producto = this.productos.find(
+      (producto) => producto.id === productoId
+    );
+    if (!producto) {
+      console.log("Producto no encontrado");
+      return null;
+    }
+    console.log("el producto fue encontrado");
+
+    return producto;
+  }
 }
 
-function generarId() {
-  let id = 0;
-
-  if (productos.length === 0) {
-    id = 1;
-  } else {
-    id = productos[productos.length - 1].id + 1;
-  }
-  return id;
-}
-
-function getProductoById(productoId) {
-  const producto = productos.find((producto) => producto.id === productoId);
-  if (!producto) {
-    console.log("Producto no encontrado");
-    return null;
-  }
-  console.log("el producto fue encontrado");
-
-  return producto;
-}
-
-// Primero se llama a getProductos, debe traer un [] array vacio.
-console.log(getProductos());
-
-// aca la llamo y agrego ese producto
-agregarProductos(
-  "producto prueba",
-  "este es un producto prueba",
+const manejadorDeProducto = new ProductMannager();
+manejadorDeProducto.agregarProductos(
+  "Producto Prueba",
+  "Este es un producto Prueba",
   200,
-  "Sin imagen",
-  "codigo123",
+  "Sin Imagen",
+  "Codigo123",
   25
 );
 
-// aca vuelvo a llamar a la funcion getProductos, esta vez deberia imprimer el primer producto que agregue
-console.log(getProductos());
+let resultado = manejadorDeProducto.getProductos();
+console.log(resultado);
 
-// aca intento agregar los mismo datos del primer producto  con la intencion de que de error, por que ya existe
-agregarProductos(
-  "producto prueba",
-  "este es un producto prueba",
+manejadorDeProducto.agregarProductos(
+  "Producto Prueba",
+  "Este es un producto Prueba",
   200,
-  "Sin imagen",
-  "codigo123",
+  "Sin Imagen",
+  "Codigo123",
   25
 );
 
-// aca en caso de que exista deberia mostrarlo, y si no marcar como null no encontrado
-console.log(getProductoById(2));
+manejadorDeProducto.agregarProductos(
+  "Producto Gomas",
+  "Este es un producto para usar haciendo deportes",
+  600,
+  "Sin Imagen",
+  "Codigo125",
+  27
+);
+
+console.log(resultado);
+let resultadoID = manejadorDeProducto.getProductoById(3);
+console.log(resultadoID);
