63c501a61a87b4158edc039f06ba03004e02b8fa
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-02-06 17:53:13 -0300

    entregable dos funcionando, pero revisar el tema de los ID "

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ class ProductMannager {
     }
   }
 
+  // consultar si hace falta sacar el await en la parte de JSON.parse
   getProductos = async () => {
     const productsfile = await fs.promises.readFile(this.path, "utf-8");
     const products = await JSON.parse(productsfile);
@@ -146,21 +147,54 @@ let cambios = {
 };
 
 async function pruebas() {
-  const manejadorDeProducto = new ProductMannager("./productos4.json");
+  const manejadorDeProducto = new ProductMannager("./productos.json");
   await manejadorDeProducto.getProductos();
+  // await manejadorDeProducto.agregarProductos(
+  //   "Porducto Prueba 50",
+  //   "Esto es un producto prueba",
+  //   300,
+  //   "sin ruta",
+  //   "Codigo1234567",
+  //   50
+  // );
+  // await manejadorDeProducto.agregarProductos(
+  //   "pantalon",
+  //   "Esto es un producto prueba",
+  //   200,
+  //   "sin ruta",
+  //   "Codigo12345",
+  //   50
+  // );
+
+  // await manejadorDeProducto.agregarProductos(
+  //   "franela",
+  //   "Esto es un producto prueba",
+  //   200,
+  //   "sin ruta",
+  //   "Codigo123456",
+  //   50
+  // );
+
+  // console.log(manejadorDeProducto.getProductoById(2));
+  manejadorDeProducto.deleteProduct(5);
+  manejadorDeProducto.deleteProduct(6);
   await manejadorDeProducto.agregarProductos(
-    "Porducto Prueba",
+    "remera",
     "Esto es un producto prueba",
     300,
     "sin ruta",
-    "Codigo123",
+    "Codigo12345678",
     50
   );
 
-  console.log(manejadorDeProducto.getProductoById(1));
-
-  manejadorDeProducto.deleteProduct(2);
-  manejadorDeProducto.updateProduct(1, cambios);
+  await manejadorDeProducto.agregarProductos(
+    "pantys",
+    "Esto es un producto prueba",
+    300,
+    "sin ruta",
+    "Codigo123456789",
+    50
+  );
 }
 
 pruebas();
